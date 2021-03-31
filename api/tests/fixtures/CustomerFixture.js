8449b495994b5f9eebd33745421d8ffa4d41eb5a
Eschechola <lucas.eschechola@outlook.com>
2021-03-31 18:49:19 -0300

    Adding create customer tests, login tests and customer fixture

---
+++ api/tests/fixtures/CustomerFixture.js
@@ -0,0 +1,20 @@
+const faker = require('faker');
+
+class CustomerFixture{
+    generateNewCustomer({
+        id = null,
+        name = null, 
+        email = null,
+        password = null
+    } = {})
+    {
+        return{
+            "id" : id != null ? id: faker.datatype.uuid(),
+            "customer_name" : name != null ? name : faker.name.findName(),
+            "customer_email" : email != null ? email : faker.internet.email(),
+            "customer_password" : password != null ? password: faker.internet.password()
+        }
+    }
+}
+
+module.exports = new CustomerFixture();
---
+++ api/tests/services/CustomerService.spec.js
@@ -0,0 +1,98 @@
+const faker = require('faker');
+const _customerFixture = require('../fixtures/CustomerFixture');
+const CustomerAlreadyExistsException = require('../../src/utilities/exceptions/CustomerAlreadyExistsException');
+
+jest.mock('../../src/infraestructure/repositories/CustomerRepository');
+const _customerRepository = require('../../src/infraestructure/repositories/CustomerRepository');
+const _customerService = require('../../src/services/CustomerService');
+
+describe("Login Tests", () => {
+    it('Sucessful login returns customer', async () => {
+        //Arrange
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+    
+        const customerByEmail = _customerFixture.generateNewCustomer({email: email, password: password}); 
+    
+        await _customerRepository.getByEmail
+            .mockResolvedValue([customerByEmail]);
+
+        //Act
+        const response = await _customerService.login(email, password);
+
+        //Assert
+        expect(response).toEqual(customerByEmail); 
+    });
+
+    it('Wrong email returns empty', async () => {
+        //Arrange
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+    
+        await _customerRepository.getByEmail
+            .mockResolvedValue([]);
+
+        //Act
+        const response = await _customerService.login(email, password);
+
+        //Assert
+        expect(response).toEqual(undefined); 
+    });
+
+    it('Wrong password returns empty', async () => {
+        //Arrange
+        const email = faker.internet.email();
+        const password = faker.internet.password();
+
+        const customerByEmail = _customerFixture.generateNewCustomer({email: email});
+        customerByEmail.password = "another_password"
+    
+        await _customerRepository.getByEmail
+            .mockResolvedValue([customerByEmail])
+
+        //Act
+        const response = await _customerService.login(email, password);
+
+        //Assert
+        expect(response).toEqual(undefined); 
+    });
+});
+
+
+describe("Create Tests", () => {
+    it("Sucessful create returns a customer", async () => {
+        //Arrange
+        const createdCustomer = _customerFixture.generateNewCustomer();
+
+        await _customerRepository.getByEmail
+            .mockResolvedValue([]);
+
+        await _customerRepository.create
+            .mockResolvedValue(createdCustomer.id);
+
+        await _customerRepository.getById
+            .mockResolvedValue([createdCustomer]);
+
+        //Act
+        const response = await _customerService.create(createdCustomer.name, createdCustomer.email, createdCustomer.password);
+
+        //Assert
+        expect(response).toEqual(createdCustomer); 
+    });
+
+    it("When customer exists throws a CustomerAlreadyExistsException", async () => {
+        //Arrange
+        const existsCustomer = _customerFixture.generateNewCustomer();
+
+        await _customerRepository.getByEmail
+            .mockResolvedValue([existsCustomer]);
+        
+        //Act 
+        const response = async () => {
+            await _customerService.create(existsCustomer.name, existsCustomer.email, existsCustomer.password);
+        };
+        
+        //Assert
+        expect(response).rejects.toEqual(CustomerAlreadyExistsException);
+    });
+});
