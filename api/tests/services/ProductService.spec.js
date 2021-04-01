6bbf8dcb4b4d37a73c0d5535140f0a2e9b132931
Eschechola <lucas.eschechola@outlook.com>
2021-04-01 01:02:23 -0300

    Adding ProductService test file

---
+++ api/tests/services/ProductService.spec.js
@@ -0,0 +1,10 @@
+const faker = require('faker');
+
+jest.mock('../../src/infraestructure/repositories/ProductRepository');
+const _productRepository = require('../../src/infraestructure/repositories/ProductRepository');
+const _productService = require('../../src/services/ProductService');
+
+
+describe("Create Tests", () => {
+    it('', async () => {});
+});
